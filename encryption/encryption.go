576a281a0387d6e96d07b9364e0e4a93faf0ced4
eze-kiel <ezekiel@fastmail.com>
2020-04-11 20:40:30 +0200

    changed panic to log

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -7,6 +7,8 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"io"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func createHash(key string) string {
@@ -19,11 +21,11 @@ func Encrypt(data []byte, passphrase string) string {
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf(err.Error())
 	}
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		log.Fatalf(err.Error())
 	}
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 	return string(ciphertext)
@@ -33,17 +35,17 @@ func Decrypt(data []byte, passphrase string) string {
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf(err.Error())
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf(err.Error())
 	}
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf(err.Error())
 	}
 	return string(plaintext)
 }
